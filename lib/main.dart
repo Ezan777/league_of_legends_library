4402fe038b1399ed002099a9cf676ba8bc6239c9
Ezan777 <enrizangri@gmail.com>
2024-04-24 14:17:16 +0200

    Locked to portrait orientation only

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:league_of_legends_library/app_bloc_observer.dart';
@@ -31,7 +32,7 @@ void main() async {
   appModel = await AppModel.initializeDataSource();
   Bloc.observer = const AppBlocObserver();
 
-  runApp(const MyApp());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
